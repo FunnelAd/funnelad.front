363afaec778da6bd2599a029de8ed279a435217d
CBarrantes24 <carlosbarrantes0524@gmail.com>
2025-08-09 17:07:23 -0500

    Update facebookSDK.jsx

--- src/core/utils/facebookSDK.jsx
+++ src/core/utils/facebookSDK.jsx
@@ -10,7 +10,7 @@ export function initFacebookSdk({ version = 'v23.0' } = {}) {
       appId            : '1355829762191593',
       autoLogAppEvents : true,
       xfbml            : true,
-      version          : 'v22.0'
+      version          : 'v19.0'
     });
       resolve(window.FB);
     };
